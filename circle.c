21b6024cd165395e4bf191c30d505ed835285938
Satyabrata Mohapatra <66768071+satya013@users.noreply.github.com>
2021-02-07 23:56:39 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<math.h>
 void main()
 {float rad,dia,cir,are;
 const float pi=3.14159;
@@ -7,7 +6,7 @@ printf("Enter radius : ");
 scanf("%f",&rad);
 dia=rad*2;
 cir=2*pi*rad;
-are=pi*pow(rad,2);
+are=pi*rad*rad;
 printf("Diameter : %.2f unit\n",dia);
 printf("Circumference : %.2f unit\n",cir);
 printf("Area : %.2f sq.unit\n",are);
