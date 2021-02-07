908909175acb0c5831cfc4e5733440d432348c88
Satyabrata Mohapatra <66768071+satya013@users.noreply.github.com>
2021-02-07 23:54:45 +0530

    Update alpha.c

--- alpha.c
+++ alpha.c
@@ -3,20 +3,24 @@ void main()
 {char c;
 printf("Enter the character : ");
 scanf("%c",&c);
-switch(c)
-{case 'a':
- case 'e':
- case 'i':
- case 'o':
- case 'u':
- case 'A':
- case 'E':
- case 'I':
- case 'O':
- case 'U':
-         printf("%c is a vowel",c);
-		 break;
-default:
- printf("%c is a consonant",c);		 	
+if((c>=65&&c<90)||(c>=97&&c<122))
+{
+    switch(c)
+        {case 'a':
+         case 'e':
+         case 'i':
+         case 'o':
+         case 'u':
+         case 'A':
+         case 'E':
+         case 'I':
+         case 'O':
+         case 'U':
+             printf("%c is a vowel",c);
+		     break;
+         default:
+             printf("%c is a consonant",c);		 	
+        }
 }
+else printf("%c is not a alphabet",c);
 }
